f1c2f1e5010312e83be69b5cc0cf5adb653236d0
okto112 <145315359+okto112@users.noreply.github.com>
2024-02-24 07:39:25 +0000

    [Add] マイページ作成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,6 +2,6 @@ class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
 
   def show
-    @customer = Customers.find(current_user.id)
+    @customer = Customer.find(current_customer.id)
   end
 end
