bbba31ee468535a0052ef73f1bb730f4d77a39c9
okto112 <145315359+okto112@users.noreply.github.com>
2024-02-25 14:14:18 +0000

    [Add] 管理者用会員情報編集機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
-devise_for :customers, skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-}
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
 
-devise_for :admin, skip: [:registrations, :passwords], controllers: {
-  sessions: "admin/sessions"
-}
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
 
 
-namespace :public do
-    get 'top', to:'homes#top'
-    get '/customers/my_page', to: 'customers#show'
-end
+  namespace :public do
+      get 'top', to:'homes#top'
+      get '/customers/my_page', to: 'customers#show'
+  end
 
-namespace :admin do
-    get 'top', to:'homes#top'
-    resources :customers, only: [:index, :show]
-end
+  namespace :admin do
+      get 'top', to:'homes#top'
+      resources :customers, only: [:index, :show, :edit, :update]
+  end
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
