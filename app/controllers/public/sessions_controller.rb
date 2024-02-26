ed390da6a490969e9b4e39e4bb6642c48b608630
okto112 <145315359+okto112@users.noreply.github.com>
2024-02-26 12:41:26 +0000

    [Add] 会員退会機能

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   def after_sign_in_path_for(resource)
@@ -38,5 +39,17 @@ class Public::SessionsController < Devise::SessionsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+  end
+
+  private
+
+  def customer_state
+    customer = Customer.find_by(email: params[:customer][:email])
+    return if customer.nil?
+    return unless customer.valid_password?(params[:customer][:password])
+    return if customer.is_active
+    unless customer.is_active
+      redirect_to new_customer_registration_path
+    end
   end
 end
