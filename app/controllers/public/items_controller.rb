da418b542ee280b4173fd9ba28d7085c687e7a62
okto112 <145315359+okto112@users.noreply.github.com>
2024-02-28 13:41:46 +0000

    [Add] 顧客用商品機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,19 @@
 class Public::ItemsController < ApplicationController
-  
+
   def index
-    @items = Item.page(params[:page])
+    if params[:item_name] != nil
+      @items = Item.where("name LIKE ?", "%#{params[:item_name]}%").page(params[:page])
+      @genres = Genre.all
+      @title = params[:item_name]
+    else
+      @items = Item.page(params[:page])
+      @genres = Genre.all
+      @title = "商品"
+    end
+  end
+
+  def show
+    @item = Item.find(params[:id])
     @genres = Genre.all
-    @title = "商品"
   end
 end
