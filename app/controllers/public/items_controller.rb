dce211851929177443e940daa5fc20771a8c01f1
okto112 <145315359+okto112@users.noreply.github.com>
2024-02-29 13:12:16 +0000

    [Add] 検索機能

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -5,6 +5,11 @@ class Public::ItemsController < ApplicationController
       @items = Item.where("name LIKE ?", "%#{params[:item_name]}%").page(params[:page])
       @genres = Genre.all
       @title = params[:item_name]
+    elsif params[:genre_id] != nil
+      @genre = Genre.find(params[:genre_id])
+      @items = @genre.items.page(params[:page])
+      @genres = Genre.all
+      @title = @genre.name
     else
       @items = Item.page(params[:page])
       @genres = Genre.all
