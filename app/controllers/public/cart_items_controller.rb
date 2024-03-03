afc51ebffd193ccfdf48e1ad65b191699b640c5a
okto112 <145315359+okto112@users.noreply.github.com>
2024-03-03 13:52:48 +0000

    [Add] カート機能作成途中

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,3 +1,24 @@
 class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
+
+  def index
+    @cart_items = Cart_item.all
+    @cart_item = Cart_item.new
+  end
+
+  def create
+    @cart_item = Cart_item.new(cart_item_params)
+    if @cart_item.save
+      redirect_to public_cart_items_path
+    else
+      @cart_item = Cart_item.new
+      render :index
+    end
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
+  end
 end
