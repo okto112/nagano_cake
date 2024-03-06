b0eaeeaee65de130aa44f2bbfb10f6a28975ddef
okto112 <145315359+okto112@users.noreply.github.com>
2024-03-06 13:39:48 +0000

    [Add] カート機能作成途中3

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -2,25 +2,33 @@ class Public::CartItemsController < ApplicationController
   before_action :authenticate_customer!
 
   def index
-    @cart_items = CartItem.all
+    @cart_items = CartItem.where(customer_id: current_customer.id)
   end
 
   def create
-    @cart_item_new = CartItem.new(cart_item_params)
-    if @cart_item_new.save
+    if CartItem.find_by(item_id: cart_item_params[:item_id], customer_id: current_customer.id)
+      @cart_item = CartItem.find_by(item_id: cart_item_params[:item_id], customer_id: current_customer.id)
+      @cart_item.amount += cart_item_params[:amount].to_i
+      @cart_item.save
       redirect_to public_cart_items_path
     else
-      @cart_items = CartItem.all
-      render :index
+      @cart_item_new = CartItem.new(cart_item_params)
+      if @cart_item_new.save
+        redirect_to public_cart_items_path
+      else
+        @cart_items = CartItem.where(customer_id: current_customer.id)
+        render :index
+      end
     end
   end
 
   def update
     @cart_item = CartItem.find(params[:id])
-    if @cart_item.update(cart_item_params)
+    byebug
+    if @cart_item.update!(cart_item_params)
       redirect_to public_cart_items_path
     else
-      @cart_items = CartItem.all
+      @cart_items = CartItem.where(customer_id: current_customer.id)
       render :index
     end
   end
@@ -32,13 +40,13 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
-    CartItem.destroy_all
+    CartItem.where(customer_id: current_customer.id).destroy_all
     redirect_to public_cart_items_path
   end
 
   private
 
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :amount)
+    params.require(:cart_item).permit(:item_id, :customer_id, :amount)
   end
 end
