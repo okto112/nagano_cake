919775f9afb229852ebef99db3fbc0184e5947e8
okto112 <145315359+okto112@users.noreply.github.com>
2024-03-09 08:39:13 +0000

    [Add] 注文情報入力作成途中2

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,5 @@
 class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting_payment: 0, payment_confirm: 1, in_production: 2, preparing_ship: 3, shipped: 4 }
   has_many :order_details, dependent: :destroy
 end
