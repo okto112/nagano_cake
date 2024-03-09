b390541fca64e0d8cb9dc213397bdde947477a7f
okto112 <145315359+okto112@users.noreply.github.com>
2024-03-09 03:22:22 +0000

    [Add] 注文情報入力作成途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,3 +1,43 @@
 class Public::OrdersController < ApplicationController
   before_action :authenticate_customer!
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if order_params[:radio] == "ご自身の住所"
+      @order.name = current_customer.last_name + current_customer.first_name
+      @order.address = current_customer.address
+      @order.postal_code = current_customer.postal_code
+    elsif order_params[:radio] == "登録済み住所から選択"
+      address = Address.find(order_params[:address_id])
+      @order.name = address.last_name + address.first_name
+      @order.address = address.address
+      @order.postal_code = address.postal_code
+    elsif order_params[:radio] == "新しく配送先を登録する"
+      new_address = Order.new(order_params)
+      if new_address.save
+        @order.name = new_address.name
+        @order.address = new_address.address
+        @order.postal_code = new_address.postal_code
+      else
+        @order = Order.new
+        render :new
+      end
+    end
+    if @order.save
+      redirect_to public_orders_confirm_path
+    else
+      @order = Order.new
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :payment_method, :total_payment, :shipping_cost ,:name, :postal_code, :address, :status, :radio ,:address_id )
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,12 +1,13 @@
 class Item < ApplicationRecord
   has_one_attached :image
   has_many :cart_items, dependent: :destroy
+  has_many :order_details, dependent: :destroy
   belongs_to :genre
-  
+
   def with_tax_price
     (price * 1.1).floor
   end
-  
+
   def get_image(width, height)
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
