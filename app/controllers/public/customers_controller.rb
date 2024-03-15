466b2f04a9080e705efb9a4c19cdebd627dfecb0
okto112 <145315359+okto112@users.noreply.github.com>
2024-03-15 07:20:47 +0000

    [Add] 顧客用会員情報編集と退会機能

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,4 +4,47 @@ class Public::CustomersController < ApplicationController
   def show
     @customer = Customer.find(current_customer.id)
   end
+
+  def edit
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def update
+    @customer = Customer.find(current_customer.id)
+    if @customer.update(customer_params)
+      redirect_to public_customers_my_page_path
+    else
+      render :edit
+    end
+  end
+
+  def unsubscribe
+    @customer = Customer.find(current_customer.id)
+  end
+
+  def withdraw
+    @customer = Customer.find(current_customer.id)
+    if @customer.update(customer_params)
+      sign_out(current_customer)
+      redirect_to new_customer_registration_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :email,
+      :postal_code,
+      :address,
+      :telephone_number,
+      :is_active
+      )
+  end
 end
